4a215d40a22fd725fcfed101269f81076215ce3a
younusvalasseri <younusv@gmail.com>
2024-07-28 06:02:39 +0530

    next

--- lib/reports/income_vs_expense_report.dart
+++ lib/reports/income_vs_expense_report.dart
@@ -9,6 +9,7 @@ class IncomeVsExpenseReport extends StatefulWidget {
   const IncomeVsExpenseReport({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _IncomeVsExpenseReportState createState() => _IncomeVsExpenseReportState();
 }
 
